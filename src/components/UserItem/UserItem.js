b7d630f21102f7ff0fef40ca882885debb3b97d7
Guilherme <gui.mochila@gmail.com>
2019-10-04 12:33:57 +0200

    Fixed <a> tag with Link

--- src/components/UserItem/UserItem.js
+++ src/components/UserItem/UserItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function UserItem({ user }) {
-  const { login, avatar_url, html_url } = user;
+  const { login, avatar_url } = user;
 
   return (
     <div className="card text-center">
@@ -15,9 +16,9 @@ function UserItem({ user }) {
       <h3>{login}</h3>
 
       <div>
-        <a href={html_url} className="btn btn-dark btn-sm my-1">
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
